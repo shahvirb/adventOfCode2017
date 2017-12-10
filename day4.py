cb65e86ed8d25091197894fc77002c6374b1245e
Shahvir <shahvirb@gmail.com>
2017-12-09 21:22:10 -0600

    adding part 2 of day 4

--- day4.py
+++ day4.py
@@ -4,18 +4,20 @@ def read_input(filename='day4.in'):
         return lines
 
 
-def valid_passphrase(words):
+def valid_passphrase(words, process):
     wordset = set()
     for word in words:
-        if word in wordset:
+        pword = process(word)
+        if pword in wordset:
             return False
-        wordset.add(word)
+        wordset.add(pword)
     return True
 
-
 if __name__ == '__main__':
-    count = 0
+    count1 = 0
+    count2 = 0
     for line in read_input():
         words = line.split()
-        count += 1 if valid_passphrase(words) else 0
-    print(count)
+        count1 += 1 if valid_passphrase(words, lambda x: x) else 0
+        count2 += 1 if valid_passphrase(words, lambda x: ''.join(sorted(x))) else 0
+    print(count1, count2)
