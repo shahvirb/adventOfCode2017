3f4e87cb2ec765f5f8674f2dd4ffb2820f9745a3
Shahvir <shahvirb@gmail.com>
2017-12-09 21:40:46 -0600

    day 5 part 2

--- day5.py
+++ day5.py
@@ -5,17 +5,22 @@ def read_input(filename='day5.in'):
 
 
 if __name__ == '__main__':
-    lines = read_input()
-    instructions = [int(inst.strip()) for inst in lines]
-    iptr = 0
-    steps = 0
-    try:
-        while True:
-            #print(iptr, instructions, steps)
-            jmp = instructions[iptr]
-            instructions[iptr] += 1
-            iptr += jmp
-            steps += 1
-    except IndexError:
-        pass
-    print(steps)
+    def solve(calc_jmp):
+        lines = read_input()
+        instructions = [int(inst.strip()) for inst in lines]
+        iptr = 0
+        steps = 0
+        try:
+            while True:
+                #print(iptr, instructions, steps)
+                jmp = instructions[iptr]
+                instructions[iptr] += calc_jmp(jmp)
+                iptr += jmp
+                steps += 1
+        except IndexError:
+            pass
+        print(steps)
+    
+    solve(lambda x: 1)
+    solve(lambda x: -1 if x >= 3 else 1)
+    
